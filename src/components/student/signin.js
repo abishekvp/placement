e80ede1b064f4163d5fb51245c3c13f41a393d18
Mohan <mohanpraveen4161@gmail.com>
2024-03-05 19:25:59 +0530

    student sign in updated

--- src/components/student/signin.js
+++ src/components/student/signin.js
@@ -4,13 +4,13 @@ import { firebaseApp } from '../../firebase';
 import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
-    const [rollnumber, setRollnumber] = useState('');
+    const [email, setEmail] = useState('');
     const [dob, setDob] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
-    const handleRollnumberChange = (e) => {
-        setRollnumber(e.target.value);
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
     };
 
     const handleDobChange = (e) => {
@@ -21,21 +21,22 @@ function SignIn() {
         e.preventDefault();
 
         try {
-            // Check if the rollnumber exists in the "Student" collection
-            const studentData = await getStudentData(rollnumber);
+            // Check if the email exists in the "Student" collection
+            const studentData = await getStudentData(email);
 
             if (!studentData) {
-                setErrorMessage('Rollnumber does not exist.');
+                setErrorMessage('Email does not exist.');
                 return;
             }
 
-            // If rollnumber exists, check if the provided dob matches
+            // If email exists, check if the provided dob matches
             if (studentData.dob === dob) {
                 console.log('Login successful!');
                 setErrorMessage('');
 
-                // Store rollnumber and role in local storage
-                localStorage.setItem('rollnumber', rollnumber);
+                // Store rollnumber, email, and role in local storage
+                localStorage.setItem('rollnumber', studentData.rollnumber);
+                localStorage.setItem('email', email);
                 localStorage.setItem('userRole', 'student');
 
                 // Navigate to /student after successful login
@@ -49,59 +50,56 @@ function SignIn() {
         }
     };
 
-    const getStudentData = async (rollnumber) => {
-        // check if rollnumber exists in the in /students route in rollnumber field
+    const getStudentData = async (email) => {
+        // Check if email exists in the /students route in the email field
         const response = await axios.get('https://placementportal.vercel.app/students');
-        const studentData = response.data.find((student) => student.rollnumber === rollnumber);
+        const studentData = response.data.find((student) => student.email === email);
         console.log('Student data:', studentData);
         return studentData;
-
     };
 
     return (
-       
-            <div className={'mainContainer'}>
-                 <div className={'signinContainer'}>
-            <div className={'titleContainer'}>
-                <div>Login</div>
-            </div>
-            
-            <br />
-            <hr/>
-            <br />
-            <div className={'inputContainer'}>
-                <input
-                    type="text"
-                    placeholder="Enter your roll number here"
-                    className={'inputBox'}
-                    value={rollnumber}
-                    onChange={handleRollnumberChange}
-                />
-                <label className="errorLabel">{errorMessage}</label>
-            </div>
-            <br />
-            <div className={'inputContainer'}>
-                <input
-                    type="date"
-                    placeholder="Enter your date of birth here"
-                    className={'inputBox'}
-                    value={dob}
-                    onChange={handleDobChange}
-                />
-                <label className="errorLabel">{errorMessage}</label>
-            </div>
-            <br />
-            <div className={'inputContainer'}>
-                <input
-                    type="button"
-                    className={'loginButton'}
-                    value={'Log in'}
-                    onClick={handleLogin}
-                />
-            </div>
+        <div className={'mainContainer'}>
+            <div className={'signinContainer'}>
+                <div className={'titleContainer'}>
+                    <div>Login</div>
+                </div>
+
+                <br />
+                <hr />
+                <br />
+                <div className={'inputContainer'}>
+                    <input
+                        type="email"
+                        placeholder="Enter your email here"
+                        className={'inputBox'}
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                    <label className="errorLabel">{errorMessage}</label>
+                </div>
+                <br />
+                <div className={'inputContainer'}>
+                    <input
+                        type="date"
+                        placeholder="Enter your date of birth here"
+                        className={'inputBox'}
+                        value={dob}
+                        onChange={handleDobChange}
+                    />
+                    <label className="errorLabel">{errorMessage}</label>
+                </div>
+                <br />
+                <div className={'inputContainer'}>
+                    <input
+                        type="button"
+                        className={'loginButton'}
+                        value={'Log in'}
+                        onClick={handleLogin}
+                    />
+                </div>
             </div>
         </div>
-        
     );
 }
 
